8450e84a09094fb49e94c336124849fe16df3758
Samuel-webalem <85428078+Samuel-webalem@users.noreply.github.com>
2023-04-24 14:48:32 +0300

    creating model for the user

--- model/usermodel.js
+++ model/usermodel.js
@@ -1,1 +1,9 @@
-//user
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Please provide a name"],
+    },
+    phonenum
+});
