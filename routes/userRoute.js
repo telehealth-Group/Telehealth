45f1158e1e79a22b7d509898b491e65a73d7ec65
Minilik94 <minilikzeru@gmail.com>
2023-04-25 12:37:09 +0300

    d

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const superAdminController = require('../controller/superAdminController')
+const express = require("express");
+const superAdminController = require("../controller/superAdminController");
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').get(superAdminController.getAllHospitals).post(superAdminController.createHospitals)
+router
+  .route("/")
+  .get(superAdminController.getAllHospitals)
+  .post(superAdminController.createHospitals);
 
-router.route('/:id').get(superAdminController.getHospital).patch(superAdminController.updateHospitals)
+router
+  .route("/:id")
+  .get(superAdminController.getHospital)
+  .patch(superAdminController.updateHospitals);
 
-module.exports = router
+module.exports = router;
