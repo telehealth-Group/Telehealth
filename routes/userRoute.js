99456b3b45e8c78ad42a237f03c305d0d0553c9e
Minilik94 <minilikzeru@gmail.com>
2023-04-25 11:43:14 +0300

    u

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const superAdminController = require('../controller/superAdminController')
 
 const router = express.Router()
 
-router.route('/').post(superAdminController.createHospitals)
+router.route('/').get(superAdminController.getAllHospitals).post(superAdminController.createHospitals)
 
 router.route('/:id').patch(superAdminController.updateHospitals)
 
