82c4ccda164fc4735d296c1caeaec00f8bcdd9ae
Minilik94 <minilikzeru@gmail.com>
2023-05-25 11:22:27 +0300

    ip

--- client/src/UI/Header.svelte
+++ client/src/UI/Header.svelte
@@ -1,9 +1,17 @@
 <script>
+    import logo from '/Logo.png'
 </script>
 
 <section class="header">
-   <nav></nav>
+
+   <nav>
+    <img src="{logo}" alt="kogo">
+   </nav>
 </section>
 
 <style>
+    img {
+        height: 150px;
+        width: 200px;
+    }
 </style>
