11e924f296a7d995ce916bdadf0918ac04b2f3c9
Samuel-webalem <85428078+Samuel-webalem@users.noreply.github.com>
2023-05-25 11:42:56 +0300

    update

--- client/src/UI/Header.svelte
+++ client/src/UI/Header.svelte
@@ -1,18 +1,30 @@
 <script>
-    import logo from '/Logo.png'
+  // You can add your script logic here
 </script>
 
 <section class="header">
-
-   <nav>
-    <img src="{logo}" alt="kogo">
-   </nav>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <nav>
+    <i class="fas fa-medkit"></i>
+    <h3>Telehealth</h3>
+  </nav>
 </section>
 
 <style>
-    img {
-        height: 150px;
-        width: 200px;
-    }
-</style>
+  section.header {
+    border: 1px solid #000;
+    padding: 20px;
+    background-color: #f5f5f5;
+  }
 
+  nav {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  h3 {
+    margin: 0;
+    font-size: 20px;
+  }
+</style>
