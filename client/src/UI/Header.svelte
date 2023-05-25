deaf725fba25ae67223f1e445688e025351a16fb
Minilik94 <minilikzeru@gmail.com>
2023-05-25 15:14:30 +0300

    e

--- client/src/UI/Header.svelte
+++ client/src/UI/Header.svelte
@@ -1,33 +1,94 @@
 <script>
-  // You can add your script logic here
+// @ts-nocheck
+
+  export let role = 'Doctor';
+  let showDropdown = false;
+
+  function logoutHandler() {
+    showDropdown = !showDropdown;
+  }
+
 </script>
 
 <section class="header">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <nav>
     <i class="fas fa-medkit"></i>
     <h3>Telehealth</h3>
   </nav>
+
+  {#if role === 'Doctor'}
+  <div class="nav--button-group">
+    <button class="nav--button-dropdown" on:click={logoutHandler}>
+      Hi Dr.Elizabeth 🙋‍♀️ Welcome <i class="fas fa-chevron-down"></i>
+    </button>
+    {#if showDropdown}
+      <div class="dropdown-content">
+        <button class="dropdown-item" onclick={logoutHandler}>Logout</button>
+      </div>
+    {/if}
+  </div>
+  {:else if role === 'Admin'}
+    <div class="nav--button-group">
+      <button class="nav--button-dropdown" on:click={logoutHandler}>
+        Hi Admin <i class="fas fa-chevron-down"></i>
+      </button>
+      {#if showDropdown}
+        <div class="dropdown-content">
+          <button class="dropdown-item" onclick={logoutHandler}>Logout</button>
+        </div>
+      {/if}
+    </div>
+  {/if}
 </section>
 
 <style>
   section.header {
     padding: 10px;
     background-color: #f5f5f5;
+    display: flex;
+    justify-content: space-between;
   }
 
-  .logo {
+  nav {
     display: flex;
     align-items: center;
     gap: 10px;
   }
 
-  .logo i {
-    font-size: 24px;
+  h3 {
+    margin: 0;
+    font-size: 20px;
   }
 
-  .logo h3 {
-    margin: 0;
-    font-size: 20px; 
+  .nav--button-group {
+    position: relative;
+  }
+
+  .nav--button-dropdown {
+    padding: 1rem;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .dropdown-content {
+    position: absolute;
+    top: 100%;
+    right: 0;
+    min-width: 120px;
+    padding: 8px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .dropdown-item {
+    display: block;
+    width: 100%;
+    padding: 4px 8px;
+    background-color: transparent;
+    border: none;
+    text-align: left;
+    cursor: pointer;
   }
 </style>
