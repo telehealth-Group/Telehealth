4388174fbf3f72979a9da15d1e1b43a111deb9a1
Minilik94 <minilikzeru@gmail.com>
2023-06-09 15:29:39 +0300

    ere

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -6,12 +6,13 @@
   import Login from "./UI/Login.svelte";
   import axios from "axios";
   import Signup from "./UI/Signup.svelte";
-  import {onMount} from 'svelte'
-  import { hospitals,appointments,patients } from './store.js';
+  import { onMount } from "svelte";
+  import { hospitals, patients } from "./store.js";
 
   let receivedData = undefined;
   let isVisible = "login";
-  let name,role
+  let name, role;
+
   function handleDataReceived(event) {
     receivedData = event.detail;
     name = receivedData.user.name;
@@ -19,31 +20,30 @@
     console.log(receivedData);
   }
 
-   async function fetchData() {
+  onMount(async () => {
     try {
-      const response = await fetch('http://127.0.0.1:3000/api/users/hospital/');
-      const { data } = await response.json();
-      const currentItems = $hospitals;
-      hospitals.set([...currentItems, ...data.hospitals]);
+      const responseHospitals = await axios.get(
+        "http://127.0.0.1:3000/api/users/hospital"
+      );
+      console.log("Hospitals:", responseHospitals.data);
+      hospitals.set(responseHospitals.data);
     } catch (error) {
       console.error(error);
     }
-  }
-
-  onMount(fetchData);
+  });
 </script>
 
 <div class="container">
   {#if receivedData === undefined && isVisible === "login"}
-    <Login
-      on:dataReceived={handleDataReceived}
-      on:signup={() => (isVisible = "signup")}
-    />
+  <Login
+    on:dataReceived={handleDataReceived}
+    on:signup={() => (isVisible = "signup")}
+  />
   {:else if receivedData === undefined && isVisible === "signup"}
-    <Signup on:signUpDataReceived={handleDataReceived}/>
+  <Signup on:signUpDataReceived={handleDataReceived} />
   {:else}
   <Header {name} {role} />
-    <Overview {role} data={receivedData}/>
+  <Overview {role} data={receivedData} />
   {/if}
 </div>
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,28 @@
-// store.js
+// Import the required dependencies
 import { writable } from 'svelte/store';
+import axios from 'axios';
 
-
+// Create writable stores for hospitals, patients, and doctors
 export const hospitals = writable([]);
-export const appointments = writable([]);
 export const patients = writable([]);
+export const doctors = writable([]);
+
+// Function to fetch data and update the store
+const fetchDataAndSetStore = async (url, store) => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    store.set(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Fetch hospitals data and update the hospitals store
+fetchDataAndSetStore('http://127.0.0.1:3000/api/hospital', hospitals);
+
+// Fetch patients data and update the patients store
+fetchDataAndSetStore('http://127.0.0.1:3000/api/patients', patients);
+
+// Fetch doctors data and update the doctors store
+fetchDataAndSetStore('http://127.0.0.1:3000/api/doctors', doctors);
