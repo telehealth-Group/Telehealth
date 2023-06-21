4304c0e981d4aab879f3cc475434253fdd961c76
Minilik94 <minilikzeru@gmail.com>
2023-06-21 12:01:54 +0300

    Updated Appointment

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -4,5 +4,7 @@ const appointmentController = require('../controller/appointmentController')
 const router = express.Router()
 
 router.route('/getAllAppointemts').get(appointmentController.getAllAppointments)
+router.route('/updateAppointment').patch(appointmentController.updateAppointment)
+router.route('/createAppointment').post(appointmentController.createAppointment)
 
 module.exports = router
