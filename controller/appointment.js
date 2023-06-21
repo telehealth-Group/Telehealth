6d449e2069111a5f412905e14862e1831b659e5a
Minilik94 <minilikzeru@gmail.com>
2023-06-21 11:28:06 +0300

    Appointemet Update

---
+++ controller/appointment.js
@@ -0,0 +1,20 @@
+const Appointment  = require("../models/appointmentModel")
+
+exports.getAllAppointments = async (req, res) =>{
+    try {
+        const appointments = await Appointment.find()
+
+        res.status(200).json({
+            status: true,
+            results: appointments.length,
+            data: {
+                appointment,
+            },
+          });
+    } catch (error) {
+        res.status(404).json({
+            status: false,
+            message: error.message,
+          });
+    }
+}
---
+++ routes/appointmentRoute.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const appointmentController = require('../controller/appointment')
+
+const route = express.Router()
+
+module.exports = route
