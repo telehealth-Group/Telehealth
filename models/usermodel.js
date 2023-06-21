fe8f7bb64a384b6accf4c64ca4d12830909fc258
Minilik94 <minilikzeru@gmail.com>
2023-06-21 12:19:24 +0300

    n

--- models/usermodel.js
+++ models/usermodel.js
@@ -57,6 +57,7 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
+ 
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
