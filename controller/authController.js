bc1d2396ddcca5390ecb3947803443e8cca4860e
Samuel-webalem <85428078+Samuel-webalem@users.noreply.github.com>
2023-07-26 14:02:47 +0300

    upk

--- controller/authController.js
+++ controller/authController.js
@@ -99,8 +99,8 @@ exports.protect = async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
-  } else if (req.cookie.jwt) {
-    token = req.cookie.jwt;
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
   if (!token) {
     return next(
